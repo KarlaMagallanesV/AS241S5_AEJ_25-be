4f9721c1818ee2b4235de7776b3c8ee2f0a92d56
KarlaMagallanesV <karla.magallanes.vicente@vallegrande.edu.pe>
2026-06-01 23:50:55 -0500

    fix(model): remove unused imports and fix file formatting in SpotifyDownloadModel

--- src/main/java/youtubeMp3/mongodb/model/SpotifyDownloadModel.java
+++ src/main/java/youtubeMp3/mongodb/model/SpotifyDownloadModel.java
@@ -1,6 +1,5 @@
-package youtubeMp3.mongodb.model;
 
-import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
