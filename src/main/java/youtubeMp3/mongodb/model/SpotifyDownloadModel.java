9ac5a13a806f4e8e6dd7ce581ebb9aedaa4e3c51
KarlaMagallanesV <karla.magallanes.vicente@vallegrande.edu.pe>
2026-06-01 23:56:13 -0500

    fix(model): add missing LocalDateTime import to SpotifyDownloadModel

--- src/main/java/youtubeMp3/mongodb/model/SpotifyDownloadModel.java
+++ src/main/java/youtubeMp3/mongodb/model/SpotifyDownloadModel.java
@@ -1,5 +1,6 @@
 package youtubeMp3.mongodb.model;
 
+import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
